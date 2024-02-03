7a74fb69425b7ea1de60b8766fe4d2bb1ce54358
Akbar Hamaminatu <akbar@ngoding.id>
2024-02-03 07:02:15 +0700

    Setup Kobweb Project

---
+++ site/src/jsMain/kotlin/id/ngoding/crypto_backend/AppEntry.kt
@@ -0,0 +1,13 @@
+package id.ngoding.crypto_backend
+
+import androidx.compose.runtime.*
+import com.varabyte.kobweb.core.App
+import com.varabyte.kobweb.core.KobwebApp
+
+@App
+@Composable
+fun AppEntry(content: @Composable () -> Unit) {
+    KobwebApp {
+        content()
+    }
+}
